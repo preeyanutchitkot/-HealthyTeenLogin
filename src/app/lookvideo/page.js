e0ea8c8b74c2f152db1394567c6fc312f3567d91
Preeyanut88888 <Preeyanut88888@gmail.com>
2025-07-27 18:09:24 +0700

    update component

---
+++ src/app/lookvideo/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '@/app/line/components/header';
+
+export default function LookvideoPage() {
+  return (
+  <div>
+      <Header title="วิดีโอสุขภาพ" cartoonImage="/9.png" />
+      {/* เนื้อหาหน้าแรก */}
+    </div>
+  );
+}
